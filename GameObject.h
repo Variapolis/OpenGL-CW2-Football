1b59db70b60d50145e29c67277b563a3103f412c
Variapolis <pma724@gmail.com>
2021-01-10 19:19:33 +0000

    Made Transform class nested in GameObject

--- GameObject.h
+++ GameObject.h
@@ -2,10 +2,23 @@
 #include <gl/freeglut.h>
 #include "ConsoleDebug.h"
 #include "Transform.h"
+#include "Vector3.h"
+
+
+
+
 class GameObject //Base class for every object, used to store position and size information.
 {
+	struct Transform
+	{
+		Vector3 position;
+		Vector3 rotation;
+		Vector3 scale;
+		Transform();
+		Transform(float x, float y, float z);
+	};
 public:
-	Transform transform; // Transform holds position, scale and rotational information.
+	Transform transform;
 	GameObject();
 	GameObject(float x, float y, float z);
 	void draw();
--- Transform.cpp
+++ Transform.cpp
@@ -1,9 +1,9 @@
-#include "Transform.h"
+#include "GameObject.h"
 
-Transform::Transform() : position(0,0,0), rotation(0, 0, 0), scale(1, 1, 1)
+GameObject::Transform::Transform() : position(0,0,0), rotation(0, 0, 0), scale(1, 1, 1)
 {
 }
 
-Transform::Transform(float x, float y, float z): position(x, y, z), rotation(0, 0, 0), scale(1, 1, 1)
+GameObject::Transform::Transform(float x, float y, float z): position(x, y, z), rotation(0, 0, 0), scale(1, 1, 1)
 {
 }
